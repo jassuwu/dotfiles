32e122c9cd2f9cb84299e40485e8bd272761e643
jassuwu <karthickprnv@gmail.com>
2024-05-01 17:19:40 +0530

    nvim: add treesitter and trouble

---
+++ .config/nvim/lua/plugins/colors.lua
@@ -0,0 +1,62 @@
+function PaintMeThePicture(paint)
+  paint = paint or "catppuccin-mocha"
+  vim.cmd.colorscheme(paint)
+
+  vim.api.nvim_set_hl(0, "Normal", { bg = 'None' })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = 'None' })
+end
+
+return {
+  {
+    "folke/tokyonight.nvim",
+    config = function()
+      require("tokyonight").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        transparent = true, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = false },
+          keywords = { italic = false },
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = "dark", -- style for sidebars, see below
+          floats = "dark", -- style for floating windows
+        },
+      })
+      -- vim.cmd.colorscheme("tokyonight")
+    end
+  },
+  {
+    "catppuccin/nvim",
+    lazy=false,
+    name="catppuccin",
+    priority=1000,
+    config=function()
+      require("catppuccin").setup({
+        disable_background = true,
+        styles = {
+          italic = false,
+        },
+      })
+
+      -- vim.cmd.colorscheme("catppuccin-mocha")
+    end
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require('rose-pine').setup({
+        disable_background = true,
+        styles = {
+          italic = false,
+        },
+      })
+
+      PaintMeThePicture("rose-pine")
+    end
+  }
+}
