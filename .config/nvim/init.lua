7fd523eee1ceb42552f8dc3f5826ca47890646d2
jassuwu <karthickprnv@gmail.com>
2024-05-01 14:42:07 +0530

    nvim: telescope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -19,7 +19,11 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.6',
+	  dependencies = { 'nvim-lua/plenary.nvim' }
+  }
 }
 local opts = {}
 
@@ -27,3 +31,6 @@ require("lazy").setup(plugins, opts)
 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
+
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
