76a1504d9c7c0ec92403fd89cd5807dec038628a
jassuwu <karthickprnv@gmail.com>
2024-07-02 08:41:18 +0530

    nvim&wezterm: revert back to rose-pine

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -26,7 +26,7 @@ return {
           floats = "dark", -- style for floating windows
         },
       })
-       PaintMeThePicture("tokyonight")
+       -- PaintMeThePicture("tokyonight")
     end
   },
   {
@@ -56,7 +56,7 @@ return {
         },
       })
 
-      -- PaintMeThePicture("rose-pine")
+      PaintMeThePicture("rose-pine")
     end
   }
 }
