25a928cf6607f6fe10f14b7add3fba3d3cf0c426
jassuwu <karthickprnv@gmail.com>
2025-02-10 10:39:52 +0530

    nvim: fix gitsigns deprecations

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -5,27 +5,24 @@ return {
     local gitsigns = require("gitsigns")
     gitsigns.setup({
       signs = {
-        add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-        change = {
-          hl = "GitSignsChange",
+        add = {
           text = "│",
-          numhl = "GitSignsChangeNr",
-          linehl = "GitSignsChangeLn",
         },
-        delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+        change = {
+          text = "│",
+        },
+        delete = {
+          text = "_",
+        },
         topdelete = {
-          hl = "GitSignsDelete",
           text = "‾",
-          numhl = "GitSignsDeleteNr",
-          linehl = "GitSignsDeleteLn",
         },
         changedelete = {
-          hl = "GitSignsChange",
           text = "~",
-          numhl = "GitSignsChangeNr",
-          linehl = "GitSignsChangeLn",
         },
-        untracked = { hl = "GitSignsAdd", text = "┆", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+        untracked = {
+          text = "┆",
+        },
       },
     })
   end
